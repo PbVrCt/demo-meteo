d2d8a010e68c9d78a80c4360059f054d472a9ed5
PbVrCt <username@pop-os.localdomain>
2024-02-02 19:10:24 +0900

    add api call from webapp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-  return <main>{/* Content will be added here */}</main>;
+import { Api } from "sst/node/api";
+
+export async function getServerSideProps() {
+  const response = await fetch(`${Api.api.url}/wind`);
+  const htmlString = await response.text();
+  return { props: { htmlString } };
+}
+
+export default function Home({ htmlString }: { htmlString: string }) {
+  return (
+    <main
+      style={{
+        display: "flex",
+        justifyContent: "left",
+      }}
+    >
+      <div style={{ width: "70%", height: "90%" }}>
+        {" "}
+        <iframe
+          srcDoc={htmlString}
+          style={{ width: "100%", height: "1000px", border: "none" }}
+        ></iframe>
+      </div>
+    </main>
+  );
 }
