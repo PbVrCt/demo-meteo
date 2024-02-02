6eb2eb05910fd1f00a2c7c87e5911a6d4b0b2864
PbVrCt <username@pop-os.localdomain>
2024-02-02 14:32:40 +0900

    remove hello world python lambda

--- src/stacks/api.ts
+++ src/stacks/api.ts
@@ -3,26 +3,6 @@ import { StackContext, Api } from "sst/constructs";
 export function API({ stack, app }: StackContext) {
   const api = new Api(stack, "api", {
     routes: {
-      "GET /hello":
-        app.mode === "dev"
-          ? {
-              function: {
-                handler:
-                  "src/packages/functions/src/python/hello/hello.handler",
-                runtime: "python3.11",
-              },
-            }
-          : {
-              function: {
-                handler: "src/packages/functions/src/python/hello",
-                runtime: "container",
-                container: {
-                  cmd: ["hello.handler"],
-                  file: "python/hello/hello.Dockerfile",
-                },
-                architecture: "x86_64",
-              },
-            },
       "GET /wind":
         app.mode === "dev"
           ? {
