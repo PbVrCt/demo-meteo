2b9fd5ac0260dbfcee000c381377677ca08e5df0
PbVrCt <username@pop-os.localdomain>
2024-02-02 21:29:19 +0900

    rename mock files

--- src/pages/api/windMock.js
+++ src/pages/api/windMock.js
@@ -4,7 +4,7 @@ import path from "path";
 export default function handler(req, res) {
   const htmlFilePath = path.join(
     process.cwd(),
-    "src/pages/tokyo_wind_speed_heatmap.html",
+    "src/pages/api/heatmapMock.html",
   );
   const htmlString = fs.readFileSync(htmlFilePath, "utf8");
   res.status(200).send(htmlString);
